15f790288a7925f0eff2d32df7193cbe633ea03c
pipelinedave <kingovi@gmail.com>
2023-09-03 13:45:23 +0000

    implemented api_key in frontend

--- frontend/main.go
+++ frontend/main.go
@@ -21,87 +21,101 @@ func main() {
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received a request.")
 	if r.Method == "POST" {
-		// // Check the API key first
-		// suppliedKey := r.FormValue("api_key") // Assuming the API key is sent in the form as "api_key"
+		log.Println("POST request received.")
 
-		// if suppliedKey != os.Getenv("MEMODAWG_KEY") {
-		// 	http.Error(w, "Invalid API key", http.StatusUnauthorized)
-		// 	return
-		// }
-
-		// Parse the form data to get the file
-		err := r.ParseMultipartForm(10 << 20) // limit your maxMultipartMemory
+		// Parsing form
+		err := r.ParseMultipartForm(10 << 20)
 		if err != nil {
+			log.Println("Failed to parse form: ", err)
 			http.Error(w, "Unable to parse form", http.StatusBadRequest)
 			return
 		}
+		log.Println("Form parsed successfully.")
 
-		// Prepare a form that you will submit to that URL.
 		var b bytes.Buffer
 		wr := multipart.NewWriter(&b)
+
+		// Processing form fields
 		for key, val := range r.PostForm {
 			for _, v := range val {
 				fw, err := wr.CreateFormField(key)
 				if err != nil {
+					log.Println("Failed to create form field: ", err)
 					http.Error(w, "Unable to parse form", http.StatusBadRequest)
 					return
 				}
 				if _, err = fw.Write([]byte(v)); err != nil {
+					log.Println("Failed to write to form field: ", err)
 					http.Error(w, "Unable to parse form", http.StatusBadRequest)
 					return
 				}
 			}
 		}
 
-		// Add the file to the form
+		log.Println("Form fields processed.")
+
+		// Processing file
 		file, _, err := r.FormFile("file")
 		if err != nil {
+			log.Println("Failed to get file: ", err)
 			http.Error(w, "Invalid file", http.StatusBadRequest)
 			return
 		}
 		defer file.Close()
+
 		fw, err := wr.CreateFormFile("file", "file")
 		if err != nil {
+			log.Println("Failed to create form file: ", err)
 			http.Error(w, "Unable to add file to form", http.StatusInternalServerError)
 			return
 		}
 		io.Copy(fw, file)
 
-		// Close the multipart writer so that the form data io.Reader is complete.
+		log.Println("File processed.")
+
+		// Close writer
 		if err := wr.Close(); err != nil {
+			log.Println("Failed to close writer: ", err)
 			http.Error(w, "Unable to add file to form", http.StatusInternalServerError)
 			return
 		}
 
-		// Create client and request
+		// Creating request
 		client := &http.Client{}
 		req, err := http.NewRequest("POST", "https://memodawg.stillon.top/transcribe", &b)
 		if err != nil {
+			log.Println("Failed to create request: ", err)
 			http.Error(w, "Error creating request", http.StatusInternalServerError)
 			return
 		}
 
-		// Set headers
+		// Adding headers
 		req.Header.Set("Content-Type", wr.FormDataContentType())
 		req.Header.Set("X-API-Key", r.FormValue("api_key"))
 
-		// Make the request
+		log.Printf("Headers set: %v", req.Header)
+
+		// Making request
 		resp, err := client.Do(req)
 		if err != nil {
+			log.Println("Failed to make request: ", err)
 			http.Error(w, "Error making request", http.StatusInternalServerError)
 			return
 		}
 		defer resp.Body.Close()
 
-		// Read and display the response
+		// Reading and displaying response
 		respBody, _ := ioutil.ReadAll(resp.Body)
+		log.Printf("Received response: %v", string(respBody))
 		w.Write(respBody)
 
 	} else {
-		// Serve the HTML form
+		log.Println("Serving HTML form.")
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
 		if err := tmpl.Execute(w, nil); err != nil {
+			log.Println("Failed to execute template: ", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
